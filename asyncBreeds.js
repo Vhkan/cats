d703203727ade13ed78e67a638872d09ad022d31
Vhkan <vhkan@yahoo.com>
2023-10-16 18:26:31 -0600

    Refactor code using async control flow

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,17 +1,18 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) functionToRunWhenThingsAreDone(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log("Return value: ", breed); //Print out details correctly
+}
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile('Bombay', printOutCatBreed);
--- syncBreeds.js
+++ syncBreeds.js
@@ -1,3 +1,5 @@
+const { log } = require("console");
+
 // data in memory
 const catBreeds = {
   'Balinese': "Balinese are curious, outgoing, intelligent cats with excellent communication skills. They are known for their chatty personalities and are always eager to tell you their views on life, love, and what you’ve served them for dinner.",
@@ -7,7 +9,7 @@ const catBreeds = {
 // synchronous function to fetch a cat breed
 const breedDetails = function(breed) {
   // can simply return it (easy peezee, butter squeezy) ...
-  return catBreeds[breed];
+   return catBreeds[breed];
 };
 
 // get the return value right away from the function
